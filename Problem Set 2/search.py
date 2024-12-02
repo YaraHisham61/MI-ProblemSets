9680a7ffb770028e1910397f794e3891b778cc04
YaraHisham61 <yara.mahrous01@eng-st.cu.edu.eg>
2024-12-02 21:05:51 +0200

    search finished but 4.5/5

--- Problem Set 2/search.py
+++ Problem Set 2/search.py
@@ -1,3 +1,4 @@
+import random
 from typing import Tuple
 from game import HeuristicFunction, Game, S, A
 from helpers.utils import NotImplemented
@@ -30,26 +31,226 @@ def greedy(game: Game[S, A], state: S, heuristic: HeuristicFunction, max_depth: 
 # get values[0].
 def minimax(game: Game[S, A], state: S, heuristic: HeuristicFunction, max_depth: int = -1) -> Tuple[float, A]:
     #TODO: Complete this function
-    if game.get_turn(state) == 0:
-        return max(minimax(game, state, heuristic, max_depth-1))
+
+    # Get the index of the current agent
+    agent_idx = game.get_turn(state)
+    
+    # Check if the current state is terminal and get the terminal values
+    terminal, terminal_values = game.is_terminal(state)
+
+    # If the state is terminal, return the terminal value for the current agent and None for the action
+    if terminal:
+        return terminal_values[agent_idx], None
+
+    # If the maximum depth is reached
+    if max_depth == 0:
+        # If it is the player's turn (max node), return the heuristic value
+        if agent_idx == 0:
+            return heuristic(game, state, agent_idx), None
+        # If it is an enemy's turn (min node), return the negative heuristic value
+        else:
+            return -heuristic(game, state, agent_idx), None
+
+    # If it is the player's turn (max node)
+    if agent_idx == 0:
+        # Return the maximum value and corresponding action from the minimax search on successor states
+        return max(((minimax(game, game.get_successor(state, action), heuristic, max_depth - 1)[0], action) 
+                   for action in game.get_actions(state)), key=lambda x: x[0])
+    # If it is an enemy's turn (min node)
     else:
-        return min(minimax(game, state, heuristic, max_depth-1))
+        # Return the minimum value and corresponding action from the minimax search on successor states
+        return min(((minimax(game, game.get_successor(state, action), heuristic, max_depth - 1)[0], action) 
+                   for action in game.get_actions(state)), key=lambda x: x[0])
 
 # Apply Alpha Beta pruning and return the tree value and the best action
 # Hint: Read the hint for minimax.
 def alphabeta(game: Game[S, A], state: S, heuristic: HeuristicFunction, max_depth: int = -1) -> Tuple[float, A]:
     #TODO: Complete this function
-    NotImplemented()
+    def alphabeta_rec(state: S, heuristic: HeuristicFunction,alpha,beta, max_depth: int = -1) -> Tuple[float, A]:
+        # Get the index of the current agent
+        agent_idx = game.get_turn(state)
+        # Check if the current state is terminal and get the terminal values
+        terminal, terminal_values = game.is_terminal(state)
+        # If the state is terminal, return the terminal value for the current agent and None for the action
+        if terminal:
+            return terminal_values[agent_idx], None
+
+        # If the maximum depth is reached
+        if max_depth == 0:
+            # If it is the player's turn (max node), return the heuristic value
+            if agent_idx == 0:
+                return heuristic(game, state, agent_idx), None
+            # If it is an enemy's turn (min node), return the negative heuristic value
+            else:
+                return -heuristic(game, state, agent_idx), None
+
+        value , best_action = None, None
+
+        # If it is the player's turn (max node)
+        if agent_idx == 0:
+            # Initialize value to -ve infinity for maximization
+            value = float('-inf')
+            # Iterate over all possible actions
+            for action in game.get_actions(state):
+                # Check the value of the child node
+                child = alphabeta_rec(game.get_successor(state, action), heuristic, alpha, beta, max_depth - 1)[0]
+                # Update value and best_action if the child value is greater than the current value
+                if child > value:
+                    value, best_action = child, action
+                # Update alpha with the maximum value
+                alpha = max(alpha, value)
+                # Prune the remaining branches if value is greater than or equal to beta
+                if value >= beta:
+                    break
+        
+        # If it is an enemy's turn (min node)
+        else:
+            # Initialize value to infinity for minimization
+            value = float('inf')
+            # Iterate over all possible actions
+            for action in game.get_actions(state):
+                # Check the value of the child node
+                child = alphabeta_rec(game.get_successor(state, action),heuristic,alpha,beta,max_depth-1)[0]
+                # Update value and best_action if the child value is less than the current value
+                if child < value:
+                    value , best_action  = child,action
+                # Update beta with the minimum value
+                beta = min(beta,value)
+                # Prune the remaining branches if value is less than or equal to alpha
+                if value <= alpha:
+                    break
+
+        return value, best_action
+    
+    ############################################################################
+
+
+
+    return alphabeta_rec(state,heuristic,float('-inf'),float('inf'),max_depth)
+    
 
 # Apply Alpha Beta pruning with move ordering and return the tree value and the best action
 # Hint: Read the hint for minimax.
 def alphabeta_with_move_ordering(game: Game[S, A], state: S, heuristic: HeuristicFunction, max_depth: int = -1) -> Tuple[float, A]:
     #TODO: Complete this function
-    NotImplemented()
+    def alphabeta_with_move_ordering_rec(state: S, heuristic: HeuristicFunction,alpha,beta, max_depth: int = -1) -> Tuple[float, A]:
+        # Get the index of the current agent
+        agent_idx = game.get_turn(state)
+        # Check if the current state is terminal and get the terminal values
+        terminal, terminal_values = game.is_terminal(state)
+        # If the state is terminal, return the terminal value for the current agent and None for the action
+        if terminal:
+            return terminal_values[agent_idx], None
+
+        # If the maximum depth is reached
+        if max_depth == 0:
+            # If it is the player's turn (max node), return the heuristic value
+            if agent_idx == 0:
+                return heuristic(game, state, agent_idx), None
+            # If it is an enemy's turn (min node), return the negative heuristic value
+            else:
+                return -heuristic(game, state, agent_idx), None
+
+        value , best_action = None, None
+        sucessors = [(action, game.get_successor(state, action)) for action in game.get_actions(state)]
+
+        # If it is the player's turn (max node)
+        if agent_idx == 0:
+            # Order successors based on heuristic value in descending order
+            sucessors_ordered = sorted(sucessors , key=lambda x: (heuristic(game, x[1], agent_idx), -sucessors.index(x)), reverse=True)
+            # Initialize value to -ve infinity for maximization
+            value = float('-inf')
+            # Iterate over all possible actions
+            for action,state in sucessors_ordered:
+                # Check the value of the child node
+                child = alphabeta_with_move_ordering_rec(state, heuristic, alpha, beta, max_depth - 1)[0]
+                # Update value and best_action if the child value is greater than the current value
+                if child > value:
+                    value, best_action = child, action
+                # Update alpha with the maximum value
+                alpha = max(alpha, value)
+                # Prune the remaining branches if value is greater than or equal to beta
+                if value >= beta:
+                    break
+        
+        # If it is an enemy's turn (min node)
+        else:
+            # Order successors based on heuristic value in ascending order
+            sucessors_ordered = sorted(sucessors , key=lambda x: (-heuristic(game, x[1], agent_idx), -sucessors.index(x)))
+            # Initialize value to infinity for minimization
+            value = float('inf')
+            # Iterate over all possible actions
+            for action,state in sucessors_ordered:
+                # Check the value of the child node
+                child = alphabeta_with_move_ordering_rec(state,heuristic,alpha,beta,max_depth-1)[0]
+                # Update value and best_action if the child value is less than the current value
+                if child < value:
+                    value , best_action  = child,action
+                # Update beta with the minimum value
+                beta = min(beta,value)
+                # Prune the remaining branches if value is less than or equal to alpha
+                if value <= alpha:
+                    break
+
+        return value, best_action
+
+    return alphabeta_with_move_ordering_rec(state,heuristic,float('-inf'),float('inf'),max_depth)
 
 # Apply Expectimax search and return the tree value and the best action
 # Hint: Read the hint for minimax, but note that the monsters (turn > 0) do not act as min nodes anymore,
 # they now act as chance nodes (they act randomly).
 def expectimax(game: Game[S, A], state: S, heuristic: HeuristicFunction, max_depth: int = -1) -> Tuple[float, A]:
     #TODO: Complete this function
-    NotImplemented()
+    def expectimax_rec(state: S, heuristic: HeuristicFunction, max_depth: int = -1) -> Tuple[float, A]:
+        # Get the index of the current agent
+        agent_idx = game.get_turn(state)
+        
+        # Check if the current state is terminal and get the terminal values
+        terminal, terminal_values = game.is_terminal(state)
+
+        # If the state is terminal, return the terminal value for the current agent and None for the action
+        if terminal:
+            return terminal_values[agent_idx], None
+
+        # If the maximum depth is reached
+        if max_depth == 0:
+            # If it is the player's turn (max node), return the heuristic value
+            if agent_idx == 0:
+                return heuristic(game, state, agent_idx), None
+            # If it is an enemy's turn (min node), return the negative heuristic value
+            else:
+                return -heuristic(game, state, agent_idx), None
+        
+        value , best_action = 0, None
+
+         # If it is the player's turn (max node)
+        if agent_idx == 0:
+            # Initialize value to -ve infinity for maximization
+            value = float('-inf')
+            # Iterate over all possible actions
+            for action in game.get_actions(state):
+                # Check the value of the child node
+                child = expectimax_rec(game.get_successor(state, action), heuristic, max_depth - 1)[0]
+                # Update value and best_action if the child value is greater than the current value
+                if child > value:
+                    value, best_action = child, action
+        
+        # If it is an enemy's turn (min node)
+        else:
+            #getting the list of possible actions
+            actions = game.get_actions(state)
+            # Iterate over all possible actions
+            for action in actions:
+                # Getting the value of the child node
+                child = expectimax_rec(game.get_successor(state, action),heuristic,max_depth-1)[0]
+                # Add the value of the child to the total value
+                value += child
+            # Calculate the average value of the children
+            value /= len(actions)
+            # Randomly select an action from the list of possible actions
+            best_action = actions[random.randint(0, len(actions) - 1)]
+
+        return value, best_action
+    
+    return expectimax_rec(state,heuristic,max_depth)
+
