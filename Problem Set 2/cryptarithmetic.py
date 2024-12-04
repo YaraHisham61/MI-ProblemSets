91f8edca3bfdaefc1a20d7365bb4945b58f3ef4d
YaraHisham61 <yara.mahrous01@eng-st.cu.edu.eg>
2024-12-04 23:28:55 +0200

    PS2 DOOONEEEEEE

--- Problem Set 2/cryptarithmetic.py
+++ Problem Set 2/cryptarithmetic.py
@@ -54,62 +54,80 @@ class CryptArithmeticProblem(Problem):
         problem.domains = {x: set(range(10)) for x in problem.variables} # add the domain of each letter to be 0-9
         problem.constraints = [BinaryConstraint((x,y) , lambda x , y : x != y) for x in problem.variables for y in problem.variables if x != y] #ensure unique value for each letter
 
-        problem.constraints.extend([UnaryConstraint(x , lambda v: v != 0) for x in set([LHS0[0], LHS1[0], RHS[0]])]) # the last letter of each term can't be zero
+        problem.constraints.extend([UnaryConstraint(x , lambda v: v != 0) for x in set([LHS0[0], LHS1[0], RHS[0]])]) # the 1st letter of each term can't be zero
 
-        aux_variables_lhs = ['{}{}'.format(LHS0[i], LHS1[i]) for i in range(-1,-min(len(LHS0), len(LHS1))-1,-1)] # aux variables for the sum of the LHS
-        problem.variables.extend(aux_variables_lhs) # adding aux variables of the LHS to the problem variables
-        problem.domains.update({x: set((d1, d2) for d1 in problem.domains.get(x[0]) for d2 in problem.domains.get(x[1])) for x in aux_variables_lhs}) # adding the domain of aux variables to be the cartesian product of d1 and d2
-
-        problem.constraints.extend([BinaryConstraint((x,x[0]), lambda xy,x: x == xy[0]) for x in aux_variables_lhs]) # adding constraints for the sum of the LHS
-        problem.constraints.extend([BinaryConstraint((x,x[1]), lambda xy,y: y == xy[1]) for x in aux_variables_lhs]) # adding constraints for the sum of the LHS
-
-        carry_list = ['C{}'.format(i) for i in range(1, min(len(LHS0), len(LHS1)))] # carry variables for each digit except the last one
-        if len(RHS) > min(len(LHS0), len(LHS1)):
-            carry_list.append('C{}'.format(len(carry_list) + 1)) # add an extra carry if RHS is longer than the LHS
-
+        carry_list = ['C{}'.format(i) for i in range(1,len(RHS))] # carry variables for each digit except the last one
         
         problem.variables.extend(carry_list) # adding carry variables to the problem variables
         problem.domains.update({c: set([0,1]) for c in carry_list}) # adding the domain of carry variables to be 0,1
 
-        aux_variables_rhs = ['{}{}'.format(RHS[len(RHS) - i - 1], c) for i,c in enumerate(carry_list)]
+        #1st aux variable = 1st digit of the LHS0 + 1st digit of the LHS1
+        aux_variables_lhs = [(LHS0[-1], LHS1[-1])] 
+
+        # adding the rest of the aux variables of the LHS to the problem variables
+        # the aux variables are the sum of the digits of the LHS and the carry
+        aux_variables_lhs.extend([(LHS0[i], LHS1[i], carry_list[-(i+2)]) for i in range(-2,-min(len(LHS0), len(LHS1))-1,-1)]) # x+y+c
+
+        # if the length of LHS0 is greater than the length of LHS1
+        # then the aux variables of the LHS will be the sum of the digits of LHS0 and the carry
+        if (len(LHS0) > len(LHS1)):
+            aux_variables_lhs.extend([(LHS0[-i], carry_list[i-2]) for i in range(len(LHS1) + 1, len(LHS0) + 1)])
+
+        problem.variables.extend(aux_variables_lhs) # adding aux variables of the LHS to the problem variables
+
+        # Adding the domain of aux variables to be the cartesian product of their respective domains
+        for x in aux_variables_lhs[1:]:
+            problem.domains[x] = set((d1, d2, d3) for d1 in problem.domains[x[0]] for d2 in problem.domains[x[1]] for d3 in problem.domains[x[-1]])
+        
+        # Adding the domain of the first element to be the cartesian product of d1 and d2
+        problem.domains[aux_variables_lhs[0]] = set((d1, d2) for d1 in problem.domains[aux_variables_lhs[0][0]] for d2 in problem.domains[aux_variables_lhs[0][1]])
+        
+        # Adding constraints for the sum of the LHS
+        for x in aux_variables_lhs[1:]:
+            problem.constraints.append(BinaryConstraint((x, x[0]), lambda xy, x: x == xy[0]))
+            problem.constraints.append(BinaryConstraint((x, x[1]), lambda xy, y: y == xy[1]))
+            problem.constraints.append(BinaryConstraint((x, x[-1]), lambda xy, y: y == xy[-1]))
+        
+        # Adding constraints for the first element
+        problem.constraints.append(BinaryConstraint((aux_variables_lhs[0], aux_variables_lhs[0][0]), lambda xy, x: xy[0] == x))
+        problem.constraints.append(BinaryConstraint((aux_variables_lhs[0], aux_variables_lhs[0][1]), lambda xy, x: xy[1] == x))
+    
+        # the rhs aux variable = rhs + carry 
+        aux_variables_rhs = [(RHS[len(RHS) - i - 1], c) for i,c in enumerate(carry_list)]
         problem.variables.extend(aux_variables_rhs) # adding aux variables of the RHS to the problem variables
-        problem.domains.update({x: set((d1, d2) for d1 in problem.domains.get(x[0]) for d2 in problem.domains.get(x[1:])) for x in aux_variables_rhs}) # adding the domain of aux variables to be the cartesian product of d1 and d2
+        problem.domains.update({x: set((d1, d2) for d1 in problem.domains.get(x[0]) for d2 in problem.domains.get(x[1])) for x in aux_variables_rhs}) # adding the domain of aux variables to be the cartesian product of d1 and d2
 
         problem.constraints.extend([BinaryConstraint((x,x[0]), lambda xy,x: x == xy[0]) for x in aux_variables_rhs]) # adding constraints for the sum of the LHS
-        problem.constraints.extend([BinaryConstraint((x,x[1:]), lambda xy,y: y == xy[1]) for x in aux_variables_rhs]) # adding constraints for the sum of the LHS
+        problem.constraints.extend([BinaryConstraint((x,x[1]), lambda xy,y: y == xy[1]) for x in aux_variables_rhs]) # adding constraints for the sum of the LHS
 
-        problem.constraints.extend([BinaryConstraint((aux_variables_lhs[i],aux_variables_rhs[i]) , lambda lhs, rhs: lhs[0] + lhs[1] == rhs[0] + 10 * rhs[1]) for i in range(min (len(aux_variables_lhs) , len(aux_variables_rhs)))]) # adding constraints for the sum of the LHS and RHS
-        # print(aux_variables_rhs)
-        # print(aux_variables_lhs)
+        # A + B = C + 10* Carry (last digit only)
+        problem.constraints.append(BinaryConstraint((aux_variables_lhs[0],aux_variables_rhs[0]) , lambda lhs, rhs: lhs[0] + lhs[1] == rhs[0] + 10 * rhs[1]))
 
-        if len(RHS) > len(LHS0) and len(LHS0) == len(LHS1):
-            problem.constraints.append(BinaryConstraint((carry_list[-1],RHS[0]) , lambda c, rhs: c == rhs))
-        
-        # problem.constraints.append(BinaryConstraint((aux_variables_lhs[-1],RHS[0]) , lambda c, rhs: c[0] + c[1] == rhs))
-        
-        # new_var = '{}{}'.format(aux_variables_lhs[-1], carry_list[-1])
-        # problem.variables.append(new_var)
-        # problem.domains[new_var] = set((d1, d2) for d1 in problem.domains.get(aux_variables_lhs[-1]) for d2 in problem.domains.get(carry_list[-1]))
-        # problem.constraints.append(BinaryConstraint((new_var,aux_variables_lhs[-1] ), lambda v, lhs: v[0] == lhs))
-        # problem.constraints.append(BinaryConstraint((new_var,carry_list[-1]) , lambda v, c: v[1] == c))
+        # (middle digits)
+        for i in range(1,len(aux_variables_rhs)):
+            if i < len (LHS1):
+                #  A + B + Carry = C + 10* Carry (middle digits)
+                problem.constraints.append(BinaryConstraint((aux_variables_lhs[i],aux_variables_rhs[i]) , lambda lhs, rhs: lhs[0] + lhs[1] + lhs[2] == rhs[0] + 10 * rhs[1]))
+            else:
+                #  A + Carry = C + 10* Carry (middle digits)
+                problem.constraints.append(BinaryConstraint((aux_variables_lhs[i],aux_variables_rhs[i]) , lambda lhs, rhs: lhs[0] + lhs[1] == rhs[0] + 10 * rhs[1]))
 
-        # problem.constraints.append(BinaryConstraint((new_var,RHS[0]) , lambda lhs, rhs: lhs[0][0] + lhs[0][1] + lhs[1] == rhs))
+        if len(LHS0) > len(LHS1) and len(RHS) == len(LHS0):
+            # if the length of LHS0 is greater than the length of LHS1 and the length of RHS is equal to the length of LHS0
+            #then the last carry + 1st of Lhs0  = the 1st digit of the RHS
+            problem.constraints.append(BinaryConstraint((aux_variables_lhs[-1],RHS[0]) , lambda lhs, rhs: lhs[0] + lhs[1] == rhs))
 
-        # def condition_repr(condition):
-        #     return dis.dis(condition)
+        if len(RHS) == len(LHS0) and len(LHS0) == len(LHS1):
+            # if the length of 2 terms of LHS is equal to the length of the RHS
+            #then the last carry + 1st of Lhs0 + 1st of lhs1  = the 1st digit of the RHS
+            problem.constraints.append(BinaryConstraint((aux_variables_lhs[-1],RHS[0]) , lambda lhs, rhs: lhs[0] + lhs[1] + lhs[2] == rhs))
 
-        # for constraint in problem.constraints:
-        #     if isinstance(constraint, UnaryConstraint):
-        #         print(f"UnaryConstraint: {constraint.variable}, {condition_repr(constraint.condition)}")
-        #     elif isinstance(constraint, BinaryConstraint):
-        #         print(f"BinaryConstraint: {constraint.variables}, {condition_repr(constraint.condition)}")
+        if len(RHS) > len(LHS0):
+            # 1st digit of RHS = last carry if the length of RHS is greater than the length of LHS0
+            problem.constraints.append(BinaryConstraint((RHS[0],carry_list[-1]) , lambda rhs, c: c == rhs))
 
-        # for constraint in problem.constraints:
-        #     if isinstance(constraint, UnaryConstraint):
-        #         print(f"UnaryConstraint: {constraint.variable}")
-        #     elif isinstance(constraint, BinaryConstraint):
-        #         print(f"BinaryConstraint: {constraint.variables}")
-        # return problem
+       
+        return problem
 
     # Read a cryptarithmetic puzzle from a file
     @staticmethod
